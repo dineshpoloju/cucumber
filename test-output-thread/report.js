57d1feb65a5a9b401cd9d2ee02590df5e7dcc4c0
poloj <poloj@DINESHKUMARP>
2021-04-17 14:21:17 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a3bc95ec-179a-4796-87a0-579089c6307b",
+    "feature": "LogIn Page Feature",
+    "scenario": "LogIn with correct credentials",
+    "start": 1615197552192,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197584284,
+    "className": "passed"
+  },
+  {
+    "id": "341d3fe9-379e-46be-b273-802f978fba24",
+    "feature": "New Product creation feature",
+    "scenario": "User tries to creates a new product with non unique product description",
+    "start": 1615197620907,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197633497,
+    "className": "passed"
+  },
+  {
+    "id": "fe9d8a58-b7e4-4382-8abe-27e9b93b051a",
+    "feature": "New Product creation feature",
+    "scenario": "User tries to creates a new product with non unique product title",
+    "start": 1615197596357,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197610063,
+    "className": "passed"
+  },
+  {
+    "id": "e78c372e-dc47-48b0-bd10-d125f8b2fbef",
+    "feature": "New Product creation feature",
+    "scenario": "User creates a new product with correct entries",
+    "start": 1615197584377,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197596277,
+    "className": "failed"
+  },
+  {
+    "id": "c7b5cfd7-88e2-4895-b210-5b15719ccadb",
+    "feature": "Delete Product feature",
+    "scenario": "Deleting a product from the product list",
+    "start": 1615197633538,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197645628,
+    "className": "passed"
+  },
+  {
+    "id": "1722a0d7-847d-4d7b-a67b-17aa46928ecd",
+    "feature": "New Product creation feature",
+    "scenario": "User tries to creates a new product with non unique product SKU",
+    "start": 1615197610087,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1615197620884,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
